cf8c3593ee5cefd8905ea2aeb486530797989f8c
chenuvi <chenuvi@outlook.com>
2020-11-06 14:55:40 +0800

    feat: 路由初步 by history router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,53 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <>
-      hello styled-components
-    </>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/money">Money</Link>
+            </li>
+            <li>
+              <Link to="/tags">Tags</Link>
+            </li>
+            <li>
+              <Link to="/statistics">Statistics</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Redirect from='/' exact to='/money'> </Redirect>
+          <Route path="/money" component={Money}></Route>
+          <Route path="/tags" component={Tags}></Route>
+          <Route path="/statistics" component={Statistics}></Route>
+          <Route path='*' component={NoMatch}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Money() {
+  return <h2>Money</h2>;
+}
+
+function Tags() {
+  return <h2>Tags</h2>;
+}
+
+function Statistics() {
+  return <h2>Statistics</h2>;
+}
+function NoMatch() {
+  return <h2>地址错误</h2>;
+}
