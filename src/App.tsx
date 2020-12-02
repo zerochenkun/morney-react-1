74a078a9be3e67fec6a9fcbaa2122c1cc9c14ddc
chenuvi <chenuvi@outlook.com>
2020-12-02 13:06:56 +0800

    Css: 导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,49 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
   Link,
   Redirect
 } from "react-router-dom";
+import styled from 'styled-components'
+const Wrapper = styled.div`
+  height:100vh;
+  display:flex;
+  flex-direction:column;
+`
+
+const Main = styled.div`
+  flex-grow:1;
+  overflow:auto;
+`
+const Nav = styled.div`
+  border:1px solid red;
+  > ul{
+    display:flex;
+    > li{
+       width:33.333333%;
+       text-align:center;
+       padding:16px;
+    }
+  }
+`
 
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Redirect from='/' exact to='/money'> </Redirect>
+            <Route path="/money" component={Money}></Route>
+            <Route path="/tags" component={Tags}></Route>
+            <Route path="/statistics" component={Statistics}></Route>
+            <Route path='*' component={NoMatch}></Route>
+          </Switch>
+        </Main>
+
+        <Nav>
           <ul>
             <li>
               <Link to="/money">Money</Link>
@@ -23,16 +55,8 @@ export default function App() {
               <Link to="/statistics">Statistics</Link>
             </li>
           </ul>
-        </nav>
-
-        <Switch>
-          <Redirect from='/' exact to='/money'> </Redirect>
-          <Route path="/money" component={Money}></Route>
-          <Route path="/tags" component={Tags}></Route>
-          <Route path="/statistics" component={Statistics}></Route>
-          <Route path='*' component={NoMatch}></Route>
-        </Switch>
-      </div>
+        </Nav>
+      </Wrapper>
     </Router>
   );
 }
