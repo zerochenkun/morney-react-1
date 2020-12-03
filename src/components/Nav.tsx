d2bc41aacfac3ec386735a9ac414cb800434fdd7
chenuvi <chenuvi@outlook.com>
2020-12-03 10:20:08 +0800

    Css: Money.tsx 之 备注 UI 完成

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,32 +4,33 @@ import { Link, NavLink } from 'react-router-dom'
 import Icon from './Icon'
 
 const NavWrapper = styled.div`
-  line-height:24px;  
-  box-shadow:0 0 3px rgba(0,0,0,0.25);
-  > ul{
-    display:flex;
-    > li{
-       width:33.333333%;
-       text-align:center;
-       padding:16px;
-       > a{
-           display:flex;
-           flex-direction:column;
-           justify-content:center;
-           align-items:center;
-           padding:4px 0;
-           .icon{
-               width:24px;
-               height:24px;
-           }
-           &.selected{
-               color:green;
-               .icon{
-                   fill:green;
-               }
-           }
-       }
-    }
+    background:white;
+    line-height:24px;  
+    box-shadow:0 0 3px rgba(0,0,0,0.25);
+    > ul{
+        display:flex;
+        > li{
+        width:33.333333%;
+        text-align:center;
+        padding:16px;
+        > a{
+            display:flex;
+            flex-direction:column;
+            justify-content:center;
+            align-items:center;
+            padding:4px 0;
+            .icon{
+                width:24px;
+                height:24px;
+            }
+            &.selected{
+                color:green;
+                .icon{
+                    fill:green;
+                }
+            }
+        }
+        }
   }
 `
 const Nav = () => {
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,7 +5,6 @@ import styled from 'styled-components'
 
 const TagsSection = styled.section`
     background:#ffffff;
-            border:1px solid red;
     padding:12px 16px;
     > ol {
         margin:0 -12px;
@@ -18,18 +17,36 @@ const TagsSection = styled.section`
             margin:8px 12px;
         }
     }
-        > button{
-            background:none;
-            border:none;
-            padding:2px 4px;
-            border-bottom:1px solid #333;
-            color:#666;
-            margin-top:8px;
-        }
-`
+    > button{
+        background:none;
+        border:none;
+        padding:2px 4px;
+        border-bottom:1px solid #333;
+        color:#666;
+        margin-top:8px;
+    }
+    `
 
 const NotesSection = styled.section`
-
+    border:1px solid red;
+    background:#f5f5f5;
+    padding:0 16px;
+    font-size:14px;
+        > label{
+            display:flex;
+            align-items:center;
+            > span {
+                margin-right:16px;
+                white-space:nowrap;
+            }
+            > input {
+                display:block;
+                width:100%;
+                height:72px;
+                background:none;
+                border:none;
+            }
+         }
 `
 const CategorySection = styled.section`
 
@@ -54,7 +71,7 @@ const Money = () => {
             <NotesSection>
                 <label>
                     <span>备注</span>
-                    <input type="text" />
+                    <input type="text" placeholder="在这里添加备注"/>
                 </label>
             </NotesSection>
             <CategorySection>
